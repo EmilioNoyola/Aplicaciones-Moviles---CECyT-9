1f5fa777634c58816c9f950a21c77d5c14e53b89
Emilio Noyola <noyola.gomez.emilio.damian@gmail.com>
2024-09-19 00:27:44 -0600

    CLASE 7 - 18/09/2024 - NOYOLA GÓMEZ EMILIO DAMIAN - 5IV9

---
+++ CLASE 7 18-09-2024/script.js
@@ -0,0 +1,43 @@
+alert("Hola desde el JS");
+//COMENTARIO DE PRUEBA SIN FUNCION ESPECIAL
+/*18 09 2024 / NOYOLA GÓMEZ EMILIO DAMIAN */
+
+/*Comentario que
+
+puede abarcar varias
+
+lineas de texto
+*/
+
+//VARIABLES
+/*
+Las variables guardan informacion
+que puedes utilizar 
+*/
+
+//DECLARACION DE VARIABLES
+let nombre = "Adam";
+const PI = 3.1416;
+var edad = 18;
+
+/*Tipos de datos
+Cadena de texto (String): "Hola", 'mundo'
+Numero (number): 5, 3, 14
+Booleanos (boolean): true, false
+*/
+
+//Ejemplo
+let mensaje = "Hola Mundo";
+let numero = 42;
+let Estudiante = true;
+
+
+//Operadores matematicos
+//igualdad
+let a = 5;
+let b = 2;
+
+let suma = a+b;//7
+let resta = a-b;//3
+let multiplicacion = a*b;//10
+let division = a/b;//2.5
